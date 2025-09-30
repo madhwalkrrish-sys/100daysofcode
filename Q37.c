867cdbdb905596f5c708843f5ff41d541b231082
krrish <madhwalkrrish@gamil.com>
2025-09-30 21:45:51 +0530

    37th commit

---
+++ Q37.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, x, y, temp, hcf, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    x = a;
+    y = b;
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    hcf = a;
+
+    lcm = (x * y) / hcf;
+
+    printf("LCM = %d\n", lcm);
+
+    return 0;
+}
