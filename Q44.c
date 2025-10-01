1c544fbaeea3b325d09619918058c53ea046355b
krrish <madhwalkrrish@gamil.com>
2025-10-01 17:43:31 +0530

    44th commit

---
+++ Q44.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    double sum = 0.0;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        sum += (double)(2*i - 1) / (2*i);
+    }
+
+    printf("Sum of the series up to %d terms = %.4f\n", n, sum);
+
+    return 0;
