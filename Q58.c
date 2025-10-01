0857b61b20812714a19fc310091c1289b5a95314
krrish <madhwalkrrish@gamil.com>
2025-10-01 18:22:59 +0530

    58th commit

---
+++ Q58.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    
+    // Ask the user for the number of elements
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Input array elements
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Initialize max and min with the first element
+    int max = arr[0];
+    int min = arr[0];
+
+    // Find max and min
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    // Print results
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d\n", min);
+
+    return 0;
+}
