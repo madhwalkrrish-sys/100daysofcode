df25194ede4383a84e37bb9dd2fd7f646c9436a7
krrish <madhwalkrrish@gamil.com>
2025-11-08 22:06:09 +0530

    71st commit

---
+++ Q71.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int matrix[10][10];
+    int rows, cols, i, j;
+
+    // Input number of rows and columns
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    // Input matrix elements
+    printf("Enter elements of the matrix (%d x %d):\n", rows, cols);
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Display the matrix
+    printf("The matrix is:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n"); // Move to next row
+    }
+
+    return 0;
+}
