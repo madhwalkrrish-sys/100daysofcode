9bd78b8138c4a77e2de4309e03f64b0a25012709
krrish <madhwalkrrish@gamil.com>
2025-11-08 21:44:05 +0530

    61st commit

---
+++ Q61.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, key, found = 0;
+    int arr[100];
+
+    // Input array size
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    // Input array elements
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input element to search
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    // Linear Search
+    for(i = 0; i < n; i++) {
+        if(arr[i] == key) {
+            printf("Element %d found at position %d.\n", key, i + 1);
+            found = 1;
+            break;
+        }
+    }
+
+    if(!found) {
+        printf("Element %d not found in the array.\n", key);
+    }
+
+    return 0;
+}
